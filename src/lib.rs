63079abe75c3c146cfd12d1e9711f5703f125110
David Tolnay <dtolnay@gmail.com>
2022-11-22 22:25:58 -0800

    Fix renamed let_underscore_drop lint
        warning: lint `clippy::let_underscore_drop` has been renamed to `let_underscore_drop`
           --> src/lib.rs:149:5
            |
        149 |     clippy::let_underscore_drop,
            |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: use the new name: `let_underscore_drop`
            |
            = note: `#[warn(renamed_and_removed_lints)]` on by default

--- src/lib.rs
+++ src/lib.rs
@@ -146,7 +146,6 @@
 #![allow(
     clippy::derive_partial_eq_without_eq,
     clippy::enum_glob_use,
-    clippy::let_underscore_drop,
     clippy::manual_find,
     clippy::match_same_arms,
     clippy::module_name_repetitions,
